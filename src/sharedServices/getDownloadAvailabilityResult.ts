a76df423ca2e210818ad1ecedff0f1b6a81d29d0
kasgds <kas.alyas@digital.cabinet-office.gov.uk>
2022-10-26 16:21:43 +0100

    removed logs

--- src/sharedServices/getDownloadAvailabilityResult.ts
+++ src/sharedServices/getDownloadAvailabilityResult.ts
@@ -24,15 +24,6 @@ export const getDownloadAvailabilityResult = async (
     currentDateEpochMilliseconds()
   )
 
-  console.log(`createdDate: ${record.createdDate}`)
-  console.log(`daysLimit: ${daysLimit}`)
-  console.log(`currentDateEpochMilliseconds: ${currentDateEpochMilliseconds()}`)
-  console.log(`numberOfDays: ${numberOfDays}`)
-  console.log(
-    `cndtion: ${numberOfDays > daysLimit && record.downloadsRemaining > 0}`
-  )
-
-  // When elapsed days is greater than allowed days
   if (numberOfDays > daysLimit && record.downloadsRemaining > 0) {
     return {
       hasAvailableDownload: record.downloadsRemaining > 0,
