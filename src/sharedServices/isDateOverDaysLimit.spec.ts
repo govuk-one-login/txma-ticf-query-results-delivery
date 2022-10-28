d01dfa454976fc6dd92f256f226a916327545daf
kasgds <kas.alyas@digital.cabinet-office.gov.uk>
2022-10-28 14:08:50 +0100

    moved mock to beforeEach

--- src/sharedServices/isDateOverDaysLimit.spec.ts
+++ src/sharedServices/isDateOverDaysLimit.spec.ts
@@ -8,8 +8,10 @@ jest.mock('../utils/currentDateEpochMilliseconds', () => ({
 describe('isDateOverDaysLimit', () => {
   const MOCK_CURRENT_TIME = 1666956229830
   const DAYS_LIMIT = 7
-  it('should return true if date is 1 millisecond over the limit', () => {
+  beforeEach(() => {
     when(currentDateEpochMilliseconds).mockReturnValue(MOCK_CURRENT_TIME)
+  })
+  it('should return true if date is 1 millisecond over the limit', () => {
     const sevenDaysAgoPlusOneMillisecond =
       MOCK_CURRENT_TIME - 1000 * 3600 * 24 * 7 - 1
     expect(
@@ -18,7 +20,6 @@ describe('isDateOverDaysLimit', () => {
   })
 
   it('should return true if date is 1 day over the limit', () => {
-    when(currentDateEpochMilliseconds).mockReturnValue(MOCK_CURRENT_TIME)
     const sevenDaysAgoPlusOneDay = MOCK_CURRENT_TIME - 1000 * 3600 * 24 * 8
     expect(isDateOverDaysLimit(sevenDaysAgoPlusOneDay, DAYS_LIMIT)).toEqual(
       true
@@ -26,13 +27,11 @@ describe('isDateOverDaysLimit', () => {
   })
 
   it('should return false if date is on the limit', () => {
-    when(currentDateEpochMilliseconds).mockReturnValue(MOCK_CURRENT_TIME)
     const sevenDaysAgoExactly = MOCK_CURRENT_TIME - 1000 * 3600 * 24 * 7
     expect(isDateOverDaysLimit(sevenDaysAgoExactly, DAYS_LIMIT)).toEqual(false)
   })
 
   it('should return false if date is within the limit by a millisecond', () => {
-    when(currentDateEpochMilliseconds).mockReturnValue(MOCK_CURRENT_TIME)
     const sevenDaysAgoMinusOneMillisecond =
       MOCK_CURRENT_TIME - 1000 * 3600 * 24 * 7 + 1
     expect(
@@ -41,7 +40,6 @@ describe('isDateOverDaysLimit', () => {
   })
 
   it('should return false if date is within the limit by a day', () => {
-    when(currentDateEpochMilliseconds).mockReturnValue(MOCK_CURRENT_TIME)
     const sixDaysAgo = MOCK_CURRENT_TIME - 1000 * 3600 * 24 * 6
     expect(isDateOverDaysLimit(sixDaysAgo, DAYS_LIMIT)).toEqual(false)
   })
