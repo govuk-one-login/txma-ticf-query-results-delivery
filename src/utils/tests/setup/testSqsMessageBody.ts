c4434b304368b4a24de361f1474b76c023081df5
mabon-gds <mabon.gwyn@digital.cabinet-office.gov.uk>
2022-11-10 16:49:22 +0000

    linting correction

--- src/utils/tests/setup/testSqsMessageBody.ts
+++ src/utils/tests/setup/testSqsMessageBody.ts
@@ -1,9 +1,6 @@
-import {
-  ZENDESK_TICKET_ID,
-  TEST_COMMENT_COPY_TEXT
-} from './testConstants'
+import { ZENDESK_TICKET_ID, TEST_COMMENT_COPY_TEXT } from './testConstants'
 
 export const testSqsMessageBody = {
   zendeskId: ZENDESK_TICKET_ID,
   commentCopyText: TEST_COMMENT_COPY_TEXT
-} 
+}
