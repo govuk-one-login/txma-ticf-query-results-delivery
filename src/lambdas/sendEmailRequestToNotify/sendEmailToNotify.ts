0730e9fea09a4b6326b147c3ac3be1e1dd3e31ab
andrewmccafferty <andrew.mccafferty@digital.cabinet-office.gov.uk>
2022-12-02 11:47:46 +0000

    Add a comment to clarify how we override the Notify base URL

--- src/lambdas/sendEmailRequestToNotify/sendEmailToNotify.ts
+++ src/lambdas/sendEmailRequestToNotify/sendEmailToNotify.ts
@@ -9,6 +9,10 @@ export const sendEmailToNotify = async (
   requestDetails: PersonalisationOptions
 ) => {
   const secrets = await retrieveNotifySecrets()
+  // The NotifyClient constructor has an undocumented feature that lets you pass in a different base URL
+  // than the production one. This lets us override the default when we want to do mocking.
+  // We discovered this by looking at the source code
+  // https://github.com/alphagov/notifications-node-client/blob/main/client/api_client.js#L17
   const notifyClient = useNotifyMockServer()
     ? new NotifyClient(getEnv('MOCK_SERVER_BASE_URL'), secrets.notifyApiKey)
     : new NotifyClient(secrets.notifyApiKey)
