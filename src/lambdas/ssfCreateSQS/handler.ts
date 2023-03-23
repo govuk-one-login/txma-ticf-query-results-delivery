87ff6850605bb620d35e8f8cb66663d0a3285add
Tom Pickering <tom.pickering@digital.cabinet-office.gov.uk>
2023-03-23 15:09:49 +0000

    tt2-434 spike add createSQS handler

---
+++ src/lambdas/ssfCreateSQS/handler.ts
@@ -0,0 +1,27 @@
+import { Context } from 'aws-lambda'
+import {
+  SQSClient,
+  CreateQueueCommand,
+  CreateQueueCommandInput
+} from '@aws-sdk/client-sqs'
+import { getEnv } from '../../utils/getEnv'
+import { initialiseLogger, logger } from '../../sharedServices/logger'
+
+export const handler = async (context: Context) => {
+  initialiseLogger(context)
+
+  const id = Date.now()
+
+  const client = new SQSClient({ region: getEnv('AWS_REGION') })
+
+  const input: CreateQueueCommandInput = {
+    QueueName: `${id}.fifo`,
+    Attributes: {
+      MessageRetentionPeriod: 86400
+    }
+  }
+
+  const result = await client.send(new CreateQueueCommand(input))
+
+  logger.info(result.QueueUrl)
+}
