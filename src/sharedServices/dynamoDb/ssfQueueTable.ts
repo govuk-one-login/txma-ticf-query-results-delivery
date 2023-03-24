b8dae3c8d366d97e251fb931b7da06a17c6c288d
Tom Pickering <tom.pickering@digital.cabinet-office.gov.uk>
2023-03-24 12:17:03 +0000

    tt2-434

--- src/sharedServices/dynamoDb/ssfQueueTable.ts
+++ src/sharedServices/dynamoDb/ssfQueueTable.ts
@@ -1,6 +1,7 @@
 import { ddbClient } from './dynamoDbClient'
 import { getEnv } from '../../utils/getEnv'
 import { PutItemCommand } from '@aws-sdk/client-dynamodb'
+import { logger } from '../logger'
 
 export const writeToQueueTable = async (
   userId: string,
@@ -15,5 +16,7 @@ export const writeToQueueTable = async (
       queueArn: { S: queueArn }
     }
   }
-  await ddbClient.send(new PutItemCommand(params))
+  const result = await ddbClient.send(new PutItemCommand(params))
+
+  logger.info(result.metadata)
 }
