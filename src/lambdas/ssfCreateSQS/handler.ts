36061cbc15eeea64c6b56f38b32132605045134d
Tom Pickering <tom.pickering@digital.cabinet-office.gov.uk>
2023-03-28 12:01:32 +0100

    queue policy

--- src/lambdas/ssfCreateSQS/handler.ts
+++ src/lambdas/ssfCreateSQS/handler.ts
@@ -16,28 +16,25 @@ export const handler = async (context: Context) => {
 
   const client = new SQSClient({ region: getEnv('AWS_REGION') })
 
-  const account = getEnv('AWS_ACCOUNT_ID')
-  const queueArn = `arn:aws:sqs:eu-west-2:${account}:ssf_sqs_${id}.fifo`
-  const policy = createPolicy(account, queueArn, id)
-
   const input: CreateQueueCommandInput = {
     QueueName: `ssf_sqs_${id}.fifo`,
     Attributes: {
       FifoQueue: 'true',
-      ContentBasedDeduplication: 'true',
-      Policy: policy
+      ContentBasedDeduplication: 'true'
     }
   }
 
   const result = await client.send(new CreateQueueCommand(input))
 
   const queueUrl = result.QueueUrl ? result.QueueUrl : ''
+  const account = getEnv('AWS_ACCOUNT_ID')
+  const queueArn = `arn:aws:sqs:eu-west-2:${account}:ssf_sqs_${id}.fifo`
 
   await writeToQueueTable(id, queueUrl, queueArn)
 
   logger.info(queueUrl)
 }
 
-const createPolicy = (account: string, queueArn: string, id: string) => {
-  return `{Version:'2012-10-17',Id:${id},Statement:[{Sid:'1',Effect:'Allow',Principal:{AWS:[${account}]},Action:['sqs:SendMessage','sqs:ReceiveMessage','sqs:DeleteMessage'],Resource:${queueArn}]}`
-}
+// const createPolicy = (account: string, queueArn: string, id: string) => {
+//   return `{Version:'2012-10-17',Id:${id},Statement:[{Sid:'1',Effect:'Allow',Principal:{AWS:[${account}]},Action:['sqs:SendMessage','sqs:ReceiveMessage','sqs:DeleteMessage'],Resource:${queueArn}]}`
+// }
