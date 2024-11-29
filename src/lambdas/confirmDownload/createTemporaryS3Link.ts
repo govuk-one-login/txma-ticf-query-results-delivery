c4a20d29515793ce93d320113adc32dfec6bb9da
Duncan Garmonsway <duncan.garmonsway@digital.cabinet-office.gov.uk>
2024-11-29 15:34:54 +0000

    TT2-1922 Externalise AWS SDK calls

--- src/lambdas/confirmDownload/createTemporaryS3Link.ts
+++ src/lambdas/confirmDownload/createTemporaryS3Link.ts
@@ -1,6 +1,6 @@
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
-import { S3Client, GetObjectCommand } from '@aws-sdk/client-s3'
-import { getEnv } from '../../utils/getEnv'
+import { GetObjectCommand } from '@aws-sdk/client-s3'
+import { s3Client } from '../../utils/awsSdkClients'
 
 const SIGNED_URL_EXPIRY_SECONDS = 300
 
@@ -8,12 +8,11 @@ export const createTemporaryS3Link = (s3BucketDetails: {
   bucket: string
   key: string
 }): Promise<string> => {
-  const client = new S3Client({ region: getEnv('AWS_REGION') })
   const command = new GetObjectCommand({
     Bucket: s3BucketDetails.bucket,
     Key: s3BucketDetails.key
   })
-  return getSignedUrl(client, command, {
+  return getSignedUrl(s3Client, command, {
     expiresIn: SIGNED_URL_EXPIRY_SECONDS
   })
 }
--- src/lambdas/generateDownload/copyDataFromAthenaOutputBucket.ts
+++ src/lambdas/generateDownload/copyDataFromAthenaOutputBucket.ts
@@ -1,9 +1,6 @@
-import {
-  CopyObjectCommand,
-  CopyObjectCommandInput,
-  S3Client
-} from '@aws-sdk/client-s3'
+import { CopyObjectCommand, CopyObjectCommandInput } from '@aws-sdk/client-s3'
 import { getEnv } from '../../utils/getEnv'
+import { s3Client } from '../../utils/awsSdkClients'
 
 export const copyDataFromAthenaOutputBucket = async (athenaQueryId: string) => {
   const copyObjectCommandInput: CopyObjectCommandInput = {
@@ -11,6 +8,5 @@ export const copyDataFromAthenaOutputBucket = async (athenaQueryId: string) => {
     Bucket: getEnv('QUERY_RESULTS_BUCKET_NAME'),
     Key: `${athenaQueryId}.csv`
   }
-  const client = new S3Client({ region: getEnv('AWS_REGION') })
-  await client.send(new CopyObjectCommand(copyObjectCommandInput))
+  await s3Client.send(new CopyObjectCommand(copyObjectCommandInput))
 }
