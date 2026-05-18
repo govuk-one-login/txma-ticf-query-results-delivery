ad08660d5419de8cd39f88668b5bb649c66c14dc
sachadorf13 <sacha.dorf@digital.cabinet-office.gov.uk>
2026-05-18 14:47:01 +0100

    Fix vitest migration issues

--- tests/integration-tests/vitest.config.integration.ts
+++ tests/integration-tests/vitest.config.integration.ts
@@ -1,15 +1,16 @@
 import { defineConfig } from 'vitest/config'
 
+process.env.STACK_NAME = 'txma-query-results'
+process.env.AWS_REGION = 'eu-west-2'
+process.env.NOTIFY_MOCK_SERVER_BASE_URL =
+  'https://mockserver.transaction.build.account.gov.uk'
+
 export default defineConfig({
   test: {
     globals: true,
     environment: 'node',
-    env: {
-      STACK_NAME: 'txma-query-results',
-      AWS_REGION: 'eu-west-2'
-    },
-    globalSetup: ['../shared-test-code/setup/setup.ts'],
-    include: ['<rootDir>/**/*.spec.ts'],
+    globalSetup: ['tests/shared-test-code/setup/setup.ts'],
+    include: ['tests/integration-tests/**/*.spec.ts'],
     testTimeout: 60000
   }
 })
